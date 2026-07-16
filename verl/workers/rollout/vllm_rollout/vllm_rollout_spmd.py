90ed573e30ce97d66d9bb3454fc70b0e324b5785
bodeqq <pumin19730829@icloud.com>
2026-07-16 12:56:10 +0800

    fix rollout teacher positions for mrope

--- verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
+++ verl/workers/rollout/vllm_rollout/vllm_rollout_spmd.py
@@ -389,12 +389,14 @@ class vLLMRollout(BaseRollout):
         response_length = response.size(1)
         delta_position_id = torch.arange(1, response_length + 1, device=position_ids.device)
         delta_position_id = delta_position_id.unsqueeze(0).expand(batch_size, -1)
-        if position_ids.dim() == 3:  # qwen2vl mrope
+        if position_ids.dim() == 3:  # qwen2vl/qwen3.5 mrope
             delta_position_id = delta_position_id.view(batch_size, 1, -1).expand(batch_size, 3, -1)
         # for teacher
         teacher_response_length = teacher_response.size(1)
         teacher_delta_position_id = torch.arange(1, teacher_response_length + 1, device=position_ids.device)
         teacher_delta_position_id = teacher_delta_position_id.unsqueeze(0).expand(batch_size, -1)
+        if position_ids.dim() == 3:  # qwen2vl/qwen3.5 mrope
+            teacher_delta_position_id = teacher_delta_position_id.view(batch_size, 1, -1).expand(batch_size, 3, -1)
 
         # TODO(sgm): fix position_ids on right_pad
         # prompt: left pad + response: right pad
